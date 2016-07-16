c4832fd258bb9dcb28efd927ea674d9e011a94f9
Maarut Chandegra <contact@maarut.net>
2016-07-16 13:00:51 +0100

    Make use of Swift code in Objective-C

--- Problem Set/GuitarString/GuitarString/main.m
+++ Problem Set/GuitarString/GuitarString/main.m
@@ -13,10 +13,16 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // a. Create an instance of the class GuitarString
+        GuitarString *string = [[GuitarString alloc] init];
         // b. Create an NSError
+        NSError *error;
         // c. Call the method pluck(velocity: Float)
+        [string pluck:100.0 error:&error];
         // d. Check if an error was returned
-        // e. Log an error if one was returned
+        if (error != nil) {
+            // e. Log an error if one was returned
+            NSLog(@"%@", error.description);
+        }
 
     }
     return 0;
